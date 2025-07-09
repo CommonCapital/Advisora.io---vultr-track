3c4225b86278f0799489cc5af4bb504d3604726b
CommonCapital <nursan2007@gmail.com>
2025-07-09 17:59:23 +0500

    your message

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,3 +1,6 @@
 import {Inngest} from "inngest";
 
-export const inngest = new Inngest({ id: "advisora"});
+export const inngest = new Inngest({ 
+    id: "advisora",
+    eventKey: process.env.INNGEST_SIGNING_KEY,
+});
